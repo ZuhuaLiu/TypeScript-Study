56649abb85bc22a1b307e2dfcbf42539cba44d83
ZuhuaLiu <liuzuhua9510@163.com>
2019-08-23 10:06:38 +0800

    to eight class

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-var fn = function () { return 'response'; };
+// let my_name: string = "rails365";
+// console.log("my name is" + my_name);
+// let myArr: number[] = [1,3];
+// console.log(myArr);
+// let myArr: Array<number> = [1,3];
+// myArr.push(2);
+// console.log(myArr);
+// let my_tuple:[string, number] = ['I hate coding', 100];
+// console.log(my_tuple[1]);
+var add = function (a, b) {
+    return a + b;
+};
+var sum = add(10, 20);
+console.log(sum);
--- index.ts
+++ index.ts
@@ -1,1 +1,27 @@
-var fn = () => 'response'
+
+// let my_name: string = "rails365";
+// console.log("my name is" + my_name);
+
+// let myArr: number[] = [1,3];
+// console.log(myArr);
+
+// let myArr: Array<number> = [1,3];
+// myArr.push(2);
+// console.log(myArr);
+
+// let my_tuple:[string, number] = ['I hate coding', 100];
+// console.log(my_tuple[1]);
+
+// const add = (a: number, b: number) => {
+//     return a + b;
+// }
+
+// let sum = add(10, 20);
+// console.log(sum);
+
+const add = (a: number, b: number) => {
+    return a + b;
+}
+
+let sum = add(10, 20);
+console.log(sum);
